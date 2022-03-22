b056addc5518a88e84c8d1d84a0d4f7b663cf9a0
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2022-03-22 22:56:59 +0000

    Parametrize tests

--- tests/octal8_test.py
+++ tests/octal8_test.py
@@ -1,49 +1,38 @@
+import pytest
+
 import octal8
 
 
-def test_main(tmp_path):
-    target = tmp_path.joinpath("target.py")
-    target.write_text(
-        """\
-from typing import Literal
-
-CONSTANT = 1234
-my_bool = True
-
-
+@pytest.mark.parametrize(
+    "original_content, new_content",
+    (
+        # Changed
+        pytest.param("CONSTANT = 1234", "CONSTANT = 0o2322", id="constant"),
+        pytest.param(
+            """\
 def foo(default=12) -> Literal[6]:
     return 6
-
-
-x = 9_000
-y = 12.34
-z = 60 * 60 * 1
-print(f"{x + 7}")
-
-EIGHT = 0o10
-"""
-    )
+""",
+            """\
+def foo(default=0o14) -> Literal[0o6]:
+    return 0o6
+""",
+            id="function-with-default-value-and-return-type",
+        ),
+        pytest.param("z = 60 * 60 * 1", "z = 0o74 * 0o74 * 0o1", id="math"),
+        pytest.param("ZERO = 0", "ZERO = 0o0", id="zero"),
+        # Unchanged
+        pytest.param("MY_FLOAT = 123.45", "MY_FLOAT = 123.45", id="float"),
+        pytest.param("EIGHT = 0o10", "EIGHT = 0o10", id="octal"),
+        pytest.param("SIXTEEN = 0x10", "SIXTEEN = 0x10", id="hex"),
+        pytest.param("MY_BOOL = True", "MY_BOOL = True", id="bool"),
+        pytest.param("print(f'{x + 7}')", "print(f'{x + 7}')", id="f-string"),
+    ),
+)
+def test_main(tmp_path, original_content, new_content):
+    target = tmp_path.joinpath("target.py")
+    target.write_text(original_content)
 
     octal8.main((str(target),))
 
-    assert (
-        target.read_text()
-        == """\
-from typing import Literal
-
-CONSTANT = 0o2322
-my_bool = True
-
-
-def foo(default=0o14) -> Literal[0o6]:
-    return 0o6
-
-
-x = 0o21450
-y = 12.34
-z = 0o74 * 0o74 * 0o1
-print(f"{x + 7}")
-
-EIGHT = 0o10
-"""
-    )
+    assert target.read_text() == new_content
